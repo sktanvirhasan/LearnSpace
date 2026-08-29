b956c8c764f979b8a2eec91d362fcc80b2a26014
S K MD TANVIR HASAN <tanvirhasan9988@gmail.com>
2026-08-29 20:45:07 +0600

    Update server.ts
    update server.ts for railway HTTP request block

--- server/config/server.ts
+++ server/config/server.ts
@@ -3,8 +3,10 @@ import type { Core } from '@strapi/strapi';
 const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Server => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('PUBLIC_URL', ''), 
-  proxy: env.bool('IS_PROXIED', false), 
+  url: env('PUBLIC_URL', ''),
+  proxy: {
+    koa: true,
+  },
   app: {
     keys: env.array('APP_KEYS')!,
   },
